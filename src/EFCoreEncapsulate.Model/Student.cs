7e3e9001f4108c000445a21635734690ab5b87a0
Piotr <piotrosz@gmail.com>
2022-09-11 18:33:25 +0200

    Add sport enrollment (not tested)

--- src/EFCoreEncapsulate.Model/Student.cs
+++ src/EFCoreEncapsulate.Model/Student.cs
@@ -5,5 +5,8 @@ public class Student
     public long Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public ICollection<Enrollment> Enrollments { get; set; }
+    public ICollection<CourseEnrollment> CourseEnrollments { get; set; }
+
+    public ICollection<SportEnrollment> SportEnrollments { get; set; }
+
 }
