8c48e70ce724d6d7e903ddff4ce5ce06897f2141
Piotr <piotrosz@gmail.com>
2022-09-16 21:30:36 +0200

    Remove antipattern

--- src/EFCoreEncapsulate.Model/Teacher.cs
+++ src/EFCoreEncapsulate.Model/Teacher.cs
@@ -4,16 +4,6 @@ public class Teacher : Entity
 {
     public Teacher(long id, string name)
     {
-        if (string.IsNullOrEmpty(name))
-        {
-            throw new ArgumentException($"Parameter „{nameof(name)}” can't be null nor empty", nameof(name));
-        }
-
-        if(name.Length > 200)
-        {
-            throw new ArgumentException($"Name can't have more than {200} characters.");
-        }
-
         Id = id;
         Name = name;
     }
